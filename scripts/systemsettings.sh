afbd6d051e9abe73e22ad6d1196f8745e19452a5
Dominik 'Lawlez' Feger <dk.feger@gmail.com>
2020-06-05 19:58:53 +0200

    Update systemsettings.sh

--- scripts/systemsettings.sh
+++ scripts/systemsettings.sh
@@ -20,7 +20,7 @@ pretty_print "#                                             #"
 pretty_print "#              SETUP OSX DEV ENV              #"
 pretty_print "#         INSTALLS EXTRAS & OSX SETTINGS      #"
 pretty_print "###############################################\n\n"
-pretty_print "this will make changes to your computer? (y/n)"
+pretty_print "this will make changes to your computer, okay? (y/n)"
 read -r response
 case $response in
   [yY]) CONTINUE=true
@@ -59,8 +59,12 @@ esac
 
 echo ""
 echo "Check for software updates daily, not just once per week"
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
+case $response in
+  [yY])
+      defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+      break;;
+  *) break;;
+esac
 
 
 ################################################################################
@@ -68,8 +72,8 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 ###############################################################################
 
 echo ""
-echo "Increasing sound quality for Bluetooth headphones/headsets"
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+#echo "Increasing sound quality for Bluetooth headphones/headsets"
+#defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 
 ###############################################################################
@@ -133,24 +137,16 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 
 
 echo ""
-echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-
-echo ""
-echo "Enabling the Develop menu and the Web Inspector in Safari"
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-
-echo "\nAdding a context menu item for showing the Web Inspector in web views"
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Use current directory as default search scope in Finder
-echo "\nUse current directory as default search scope in Finder"
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+echo "Do you want to enable snap-to-grid for icons on the desktop and in other icon views? (y/n)"
+read -r response
+case $response in
+  [yY])
+    /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+    /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+    /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+    break;;
+  *) break;;
+esac
 
 # Show Path bar in Finder
 echo "\nShow Path bar in Finder"
@@ -165,22 +161,31 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Show indicator lights for open applications in the Dock
 echo "\nShow indicator lights for open applications in the Dock"
 defaults write com.apple.dock show-process-indicators -bool true
-echo "Safari - Hide bookmarks bar"
-defaults write com.apple.Safari ShowFavoritesBar -bool false
 
+echo "\n Enable Dark mode"
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark" ##darkmode
+
+echo "\n Switch accent color"
 defaults write NSGlobalDomain AppleHighlightColor -string "0.152941 0.964706 0.978431" ##accentcolor
+
+echo "\n turn off mouse scaling"
 defaults write NSGlobalDomain com.apple.mouse.scaling -int 1 ##mousescaling
+
+echo "\n enable beeponerror"
 defaults write NSGlobalDomain com.apple.sound.beep.flash -int 1 ##beeponerror
-defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName ##login info in lockscreeen
+
+#defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName ##login info in lockscreeen
+
+echo "\n Switch accent color"
 defaults write -g ApplePressAndHoldEnabled -bool false ##disable press ndhold for keyrepeatt
-# Set a blazingly fast keyboard repeat rate
-#echo "\nSet a blazingly fast keyboard repeat rate"
-#defaults write NSGlobalDomain KeyRepeat -int 1
+
+Set a blazingly fast keyboard repeat rate
+echo "\nSet a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 1
 
 # Set a shorter Delay until key repeat
-#echo "\nSet a shorter Delay until key repeat"
-#defaults write NSGlobalDomain InitialKeyRepeat -int 12
+echo "\nSetting a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 # Show the ~/Library folder
 echo "\nShow the ~/Library folder"
@@ -198,12 +203,25 @@ defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 echo "Disable window resume system-wide"
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
-echo "Disable auto-correct"
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+echo "Disable auto-correct?"
+read -r response
+case $response in
+  [yY])
+   defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+    break;;
+  *) break;;
+esac
 
-echo "Require password immediately after sleep or screen saver begins"
-defaults write com.apple.screensaver askForPassword -int 1
+
+echo "Require password immediately after sleep or screen saver begins?"
+read -r response
+case $response in
+  [yY])
+   defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
+    break;;
+  *) break;;
+esac
 
 echo "Keyboard - Automatically illuminate built-in MacBook keyboard in low light"
 defaults write com.apple.BezelServices kDim -bool true
@@ -211,19 +229,37 @@ defaults write com.apple.BezelServices kDim -bool true
 echo "Keyboard - Turn off keyboard illumination when computer is not used for 15 minutes"
 defaults write com.apple.BezelServices kDimTime -int 900
 
-echo "Trackpad - Map bottom right corner to right-click"
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+echo "Trackpad - Map bottom right corner to right-click?"
+read -r response
+case $response in
+  [yY])
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+    defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+    defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+    break;;
+  *) break;;
+esac
 
-echo "Trackpad - Enable tap to click for current user and the login screen"
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 
+echo "Trackpad - Enable tap to click for current user and the login screen?"
+read -r response
+case $response in
+  [yY])
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+    defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 
+    break;;
+  *) break;;
+esac
 
-echo "Chrome - Prevent native print dialog, use system dialog instead"
-defaults write com.google.Chrome DisablePrintPreview -boolean true
+echo "Chrome - Prevent native print dialog, use system dialog instead?"
+read -r response
+case $response in
+  [yY])
+   defaults write com.google.Chrome DisablePrintPreview -boolean true
+    break;;
+  *) break;;
+esac
 
 echo "TextEdit - Use plain text mode for new documents"
 defaults write com.apple.TextEdit RichText -int 0
@@ -248,7 +284,7 @@ echo ""
 
 find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-  "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
-  "Terminal" "Transmission"; do
+  "Dock" "Finder" "Messages" "SystemUIServer" \
+  "Terminal"; do
   killall "${app}" > /dev/null 2>&1
 done
